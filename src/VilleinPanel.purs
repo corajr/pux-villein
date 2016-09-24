56662935b2df19e7eebb5602125ba7eb0ac24464
Cora Johnson-Roberson <me@corajr.com>
2016-09-24 19:54:28 -0400

    model works, needs display

--- src/VilleinPanel.purs
+++ src/VilleinPanel.purs
@@ -1,15 +1,18 @@
 module App.VilleinPanel where
 
 import Prelude
-import Pux.Html (Html, div, span, input, text)
-import Pux.Html.Attributes (type_, value)
-import Pux.Html.Events (onClick)
-import Data.Map (Map)
-import Data.Map as Map
-import Data.Maybe (Maybe(..))
 import Data.Array as Array
-import Data.Tuple (Tuple(..))
+import Data.Map as Map
+import Pux.Html as Html
 import Data.Generic (class Generic, gShow)
+import Data.Int as Int
+import Data.Map (Map)
+import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
+import Data.Unfoldable (unfoldr)
+import Pux.Html (Html, span, input, text)
+import Pux.Html.Attributes (offset, type_, value, checked)
+import Pux.Html.Events (onClick, onChange, FormEvent)
 
 newtype Coord = Coord
   { x :: Int
@@ -42,11 +45,16 @@ instance showEdge :: Show Edge where
   show = gShow
 
 data Action = Toggle Edge
+            | SetToNum Int
+            | NumberChange FormEvent
             | Reset
 
 type Edges = Map Edge Boolean
 
-type State = Edges
+type State =
+  { edges :: Edges
+  , number :: Int
+  }
 
 gridOfSize :: Int -> Edges
 gridOfSize n = Map.fromFoldable <<< Array.concat $ do
@@ -58,7 +66,7 @@ gridOfSize n = Map.fromFoldable <<< Array.concat $ do
   pure $ [Tuple e1 false, Tuple e2 false]
 
 init :: State
-init = gridOfSize 2
+init = { edges: gridOfSize 2, number: 0}
 
 mkEdge :: Int -> Int -> Direction -> Coord -> Edge
 mkEdge dx dy dir (Coord {x: x, y: y}) =
@@ -82,18 +90,51 @@ digit3 c =
   , mkEdge 0 0 North c
   ]
 
+digit :: Tuple Int Coord -> Array Edge
+digit (Tuple i c) =
+  case i of
+    0 -> []
+    1 -> digit1 c
+    2 -> digit2 c
+    3 -> digit3 c
+    _ -> []
+
+coords :: Array Coord
+coords = [ Coord {x: 0, y: 0} -- ones
+         , Coord {x: -1, y: 1} -- fours
+         , Coord {x: 1, y: 1} -- sixteens
+         , Coord {x: 1, y: -1} -- sixty-fours
+         ]
+
+-- Produces digits in reverse order, suitable to zip with coords
+toBase :: Int -> Int -> Array Int
+toBase base n = unfoldr f n
+  where f 0 = Nothing
+        f x = Just (Tuple (x `mod` base) (x `div` base))
+
+toEdges :: Int -> Map Edge Boolean
+toEdges n = Map.unionWith (||) activeEdges init.edges
+  where activeEdges = Map.fromFoldable (map (\x -> Tuple x true) (Array.concatMap digit (Array.zip digits coords)))
+        digits = toBase 4 n
+
 update :: Action -> State -> State
-update (Toggle edge) state = Map.update (Just <<< not) edge state
+update (Toggle edge) state = state { edges = Map.update (Just <<< not) edge state.edges }
+update (NumberChange ev) state =
+  case (Int.fromString ev.target.value) of
+    Just n -> update (SetToNum n) state
+    Nothing -> state
+update (SetToNum n) state = state { number = n, edges = toEdges n}
 update Reset state = init
 
 view :: State -> Html Action
 view state =
-  div
+  Html.div
     []
-    ((map checkbox <<< Array.fromFoldable <<< Map.toList $ state) <>
-    [ span [] [ text (show state) ]
-    ])
+    ([input [ type_ "text", value (show state.number), onChange NumberChange] []] <>
+    ((map checkbox <<< Array.fromFoldable <<< Map.toList $ state.edges) <>
+    [ Html.div [] [ text (show (toBase 4 state.number))]
+    ]))
 
 
 checkbox :: Tuple Edge Boolean -> Html Action
-checkbox (Tuple e b) = input [ type_ "checkbox", value (show b), onClick (const (Toggle e))] []
+checkbox (Tuple e b) = input [ type_ "checkbox", checked b, onClick (const (Toggle e))] []
